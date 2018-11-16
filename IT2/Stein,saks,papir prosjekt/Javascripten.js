20f661b9551e1e50a97ad5ac3516e06bf1cb025a
Havra Ismailova <havra.xd@hotmail.com>
2018-11-16 13:10:11 +0100

    uke 42, 2018 IT2

--- IT2/Stein,saks,papir prosjekt/Javascripten.js
+++ IT2/Stein,saks,papir prosjekt/Javascripten.js
@@ -1,80 +1,101 @@
-var brukerScore = 0;
-var computerScore = 0;
-var brukerScore_span = document.getElementById("user-score");
+let userScore = 0;
+let computerScore = 0;
+var userScore_span = document.getElementById("user-score");
 var computerScore_span = document.getElementById("computer-score");
-var stillingen_div = document.querySelector(".stillingen");
-var result_p = document.querySelector(".resultat");
+var scoreboard_div = document.querySelector(".score-board");
+var result_p = document.querySelector(".result");
 var Rock_div = document.getElementById("r");
 var Scissors_div = document.getElementById("s");
 var Paper_div = document.getElementById("p");
+var wrong = document.getElementById("wrong");
+var correct = document.getElementById("right");
 
 
-function getcomputerChoice() {
-    const valgene = ['stein','saks','papir'];
+function riktigLyd(){
+    correct.play();
+}
+
+function feilLyd(){
+    wrong.play();
+}
+
+
+
+
+function getComputerChoice() {
+    const valgene = ['r','p','s'];
     const randomNummer = Math.floor(Math.random() * 3);
     return valgene [randomNummer];
 }
 
-function endreOrd(letter) {
+function endreTilOrd(letter) {
     if (letter === "r")return "Rock";
     if (letter === "p")return "Paper";
     return "Scissors";
-
 }
 
-function win(brukerChoice, computerChoice) {
-    brukerScore++;
-    brukerScore_span.innerHTML = brukerScore;
+function win(userChoice, computerChoice) {
+    const underOrdBruker = "user".fontsize(3).sub();
+    const underOrdComp = "comp".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
+    userScore++;
+    userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `${endreOrd(brukerChoice)} smashes ${endreOrd(computerChoice)}. You win!`;
+    result_p.innerHTML = `${endreTilOrd(userChoice)}${underOrdBruker} smashes ${endreTilOrd(computerChoice)}${underOrdComp}. You win!`;
+    riktigLyd();
 }
 
-function lose() {
+function lose(userChoice, computerChoice) {
+    const underOrdBruker = "user".fontsize(3).sub();
+    const underOrdComp = "comp".fontsize(3).sub();
+    const userChoice_div = document.getElementById(userChoice);
     computerScore++;
-    brukerScore_span.innerHTML = brukerScore;
+    userScore_span.innerHTML = userScore;
     computerScore_span.innerHTML = computerScore;
-    result_p.innerHTML = `${endreOrd(brukerChoice)} loses to ${endreOrd(computerChoice)}. Better luck next time!`;
+    result_p.innerHTML = `${endreTilOrd(userChoice)}${underOrdBruker} loses to ${endreTilOrd(computerChoice)}${underOrdComp}. Better luck next time!`;
+    feilLyd();
 }
 
 function draw() {
-    result_p.innerHTML = `${endreOrd(brukerChoice)} equals to ${endreOrd(computerChoice)}. Both of you lose!`;
+    const userChoice_div = document.getElementById(userChoice);
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const underOrdBruker = "user".fontsize(3).sub();
+    const underOrdComp = "comp".fontsize(3).sub();
+    result_p.innerHTML = `${endreTilOrd(userChoice)}${underOrdBruker} equals to ${endreTilOrd(computerChoice)}${underOrdComp}. It's a draw!`;
 }
 
-function spill(brukerChoice) {
-    var computerChoice = getcomputerChoice();
-    //console.log("bruker valg =>" + brukerChoice); // tester i konsollen
-    //console.log("pc valg =>" + computerChoice); // tester i konsollen
-
-    switch (brukerChoice + computerChoice) {
-        case "r s":
-        case "p r":
-        case "s p":
-            win();
+function spill(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (computerChoice + userChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
             break;
-        case "r p":
-        case "p s":
-        case "s r":
-            lose();
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
             break;
-        case "r r":
-        case "s s":
-        case "p p":
-            draw();
+        case "rr":
+        case "ss":
+        case "pp":
+            draw(userChoice, computerChoice);
             break;
+        //console.log("bruker valg =>" + userChoice); // tester i konsollen
+        //console.log("pc valg =>" + computerChoice); // tester i konsollen
     }
 }
 
-
 function main() {
-    Rock_div.addEventListener('click', function () {
-        spill("r");
-    })
-    Scissors_div.addEventListener('click', function () {
-        spill("s");
-    })
-    Paper_div.addEventListener('click', function () {
-        spill("p");
-    })
+    Rock_div.addEventListener('click', () => spill("r"));
+    Scissors_div.addEventListener('click', () => spill("s"));
+    Paper_div.addEventListener('click', () => spill("p"));
 }
 
 main();
+
+document.getElementById("reset-game").onclick = function() {
+    document.location.href = "";
+}
